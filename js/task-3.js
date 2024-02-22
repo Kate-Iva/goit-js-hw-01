e737ed500bc269a0f8fb97611de22aeb8c860e06
Kate-Iva <152105481+Kate-Iva@users.noreply.github.com>
2024-02-22 19:56:11 +0000

    hw_1

---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+function getElementWidth(content, padding, border){
+    const contentWidth = Number.parseFloat(content);
+    const paddingWidth = Number.parseFloat(padding);
+    const borderWidth = Number.parseFloat(border);
+    const paddingWidthAll = (paddingWidth + paddingWidth)
+    const borderWidthAll = (borderWidth + borderWidth)
+
+    return contentWidth + paddingWidthAll + borderWidthAll;
+
+}
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
